109f4fd4df4c7ebb0898ca029f5ff7f4d4d41278
Tachy Artist <you@example.com>
2026-05-29 14:43:53 +0700

    fix: hardcode GTM ID, suppressHydrationWarning on html

--- src/app/layout.js
+++ src/app/layout.js
@@ -76,7 +76,7 @@ export default function RootLayout({ children }) {
       <body suppressHydrationWarning>
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K9T7TND3"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
@@ -91,7 +91,7 @@ export default function RootLayout({ children }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
+})(window,document,'script','dataLayer','GTM-K9T7TND3');`,
           }}
         />
       </body>
