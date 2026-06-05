dfae7e89f9c7af0e45f7fa7b7307d07055c8a500
Tachy Artist <you@example.com>
2026-06-05 13:06:59 +0700

    allow ai crawlers in robots

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,8 +1,27 @@
 import { siteMetadata } from "@/lib/seo"
 
+const aiAndSearchBots = [
+  "Googlebot",
+  "Google-Extended",
+  "GPTBot",
+  "ChatGPT-User",
+  "OAI-SearchBot",
+  "ClaudeBot",
+  "CCBot",
+  "PerplexityBot",
+  "Bytespider",
+  "meta-externalagent",
+  "Amazonbot",
+  "Applebot",
+  "Applebot-Extended",
+]
+
 export default function robots() {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      { userAgent: "*", allow: "/" },
+      ...aiAndSearchBots.map((bot) => ({ userAgent: bot, allow: "/" })),
+    ],
     sitemap: [
       `${siteMetadata.siteUrl}/sitemap.xml`,
       `${siteMetadata.siteUrl}/post_sitemap.xml`,
